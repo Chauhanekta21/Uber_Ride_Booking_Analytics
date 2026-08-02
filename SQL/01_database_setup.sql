59eef284a8ff5d982e2484af7792779949eaf10a
Chauhanekta21 <chauhanekta31@gmail.com>
2026-08-02 16:05:37 +0530

    Removed unnecessary files, added final uber thumbnail png file, and renamed sql files

--- SQL/01_database_setup.sql
+++ SQL/01_database_setup.sql
@@ -1,102 +1,39 @@
--- STEP02: Inspected the raw dataset for structure, data quality, and consistency.
+-- STEP01: Created the raw_uber_bookings table and imported the original Uber Ride Booking dataset into PostgreSQL.
+
+CREATE TABLE IF NOT EXISTS raw_uber_bookings (
+    booking_date DATE,
+    booking_time TIME,
+    booking_id VARCHAR(50),
+    booking_status VARCHAR(50),
+    
+    customer_id VARCHAR(50),
+    
+    vehicle_type VARCHAR(50),
+    pickup_location VARCHAR(100),
+    drop_location VARCHAR(100),
+    
+    avg_vtat DECIMAL(5,2),
+    avg_ctat DECIMAL(5,2),
+    
+    cancelled_rides_by_customer INT,
+    customer_cancellation_reason TEXT,
+    
+    cancelled_rides_by_driver INT,
+    driver_cancellation_reason TEXT,
+    
+    incomplete_rides INT,
+    incomplete_rides_reason TEXT,
+    
+    booking_value DECIMAL(10,2),
+    ride_distance DECIMAL(10,2),
+    
+    driver_rating DECIMAL(3,2),
+    customer_rating DECIMAL(3,2),
+    
+    payment_method VARCHAR(50)
+);
 
 
--- Step 01: Preview the dataset
-SELECT *
+SELECT * 
 FROM raw_uber_bookings;
 
-
-
--- Step 02: Count total records
-SELECT COUNT(*) AS total_records
-FROM raw_uber_bookings;
-
-
-
--- Step 03: View table structure
-SELECT *
-FROM information_schema.columns
-WHERE TABLE_NAME = 'raw_uber_bookings';
-
-
-
--- Step 04: Check column data types
-SELECT column_name, data_type
-FROM information_schema.columns
-WHERE TABLE_NAME = 'raw_uber_bookings';
-
-
-
--- Step 05: Check missing values
-SELECT
-    COUNT(*) AS total_rows,
-    COUNT(*) - COUNT(booking_date) AS date_nulls,
-    COUNT(*) - COUNT(booking_time) AS time_nulls,
-    COUNT(*) - COUNT(booking_id) AS booking_id_nulls,
-    COUNT(*) - COUNT(booking_status) AS booking_status_nulls,
-    COUNT(*) - COUNT(customer_id) AS customer_id_nulls,
-    COUNT(*) - COUNT(vehicle_type) AS vehicle_type_nulls,
-    COUNT(*) - COUNT(pickup_location) AS pickup_location_nulls,
-    COUNT(*) - COUNT(drop_location) AS drop_location_nulls,
-    COUNT(*) - COUNT(avg_vtat) AS avg_vtat_nulls,
-    COUNT(*) - COUNT(avg_ctat) AS avg_ctat_nulls,
-    COUNT(*) - COUNT(cancelled_rides_by_customer) AS cancelled_by_customer_nulls,
-    COUNT(*) - COUNT(customer_cancellation_reason) AS customer_reason_nulls,
-    COUNT(*) - COUNT(cancelled_rides_by_driver) AS cancelled_by_driver_nulls,
-    COUNT(*) - COUNT(driver_cancellation_reason) AS driver_reason_nulls,
-    COUNT(*) - COUNT(incomplete_rides) AS incomplete_rides_nulls,
-    COUNT(*) - COUNT(incomplete_rides_reason) AS incomplete_reason_nulls,
-    COUNT(*) - COUNT(booking_value) AS booking_value_nulls,
-    COUNT(*) - COUNT(ride_distance) AS ride_distance_nulls,
-    COUNT(*) - COUNT(driver_rating) AS driver_ratings_nulls,
-    COUNT(*) - COUNT(customer_rating) AS customer_rating_nulls,
-    COUNT(*) - COUNT(payment_method) AS payment_method_nulls
-FROM raw_uber_bookings;
-
-
-
--- Step 06: Check blank values in text columns
-
-SELECT
-    COUNT(*) FILTER (WHERE TRIM(booking_id) = '') AS booking_id_blank,
-    COUNT(*) FILTER (WHERE TRIM(booking_status) = '') AS booking_status_blank,
-    COUNT(*) FILTER (WHERE TRIM(customer_id) = '') AS customer_id_blank,
-    COUNT(*) FILTER (WHERE TRIM(vehicle_type) = '') AS vehicle_type_blank,
-    COUNT(*) FILTER (WHERE TRIM(pickup_location) = '') AS pickup_location_blank,
-    COUNT(*) FILTER (WHERE TRIM(drop_location) = '') AS drop_location_blank,
-    COUNT(*) FILTER (WHERE TRIM(Customer_cancellation_reason) = '') AS customer_reason_blank,
-    COUNT(*) FILTER (WHERE TRIM(driver_cancellation_reason) = '') AS driver_reason_blank,
-    COUNT(*) FILTER (WHERE TRIM(incomplete_rides_reason) = '') AS incomplete_reason_blank,
-    COUNT(*) FILTER (WHERE TRIM(payment_method) = '') AS payment_method_blank
-FROM raw_uber_bookings;
-
-
-
--- Step 07: Check duplicate records
-SELECT COUNT(*) AS total_rows, COUNT(DISTINCT(booking_id)) AS distinct_rows, 
-	   COUNT(*) - COUNT(DISTINCT(booking_id)) AS duplicate_rows
-FROM raw_uber_bookings;
-
-
-
--- Step 08: Check for exact duplicate rows
-
-SELECT *,
-       COUNT(*) AS duplicate_count
-FROM raw_uber_bookings
-GROUP BY
-    booking_date, booking_time, booking_id, booking_status, customer_id,
-    vehicle_type, pickup_location, drop_location,
-    avg_vtat, avg_ctat,
-    cancelled_rides_by_customer,
-    customer_cancellation_reason,
-    cancelled_rides_by_driver,
-    driver_cancellation_reason,
-    incomplete_rides,
-    incomplete_rides_reason,
-    booking_value,
-    ride_distance,
-    driver_rating,
-    customer_rating,
-    payment_method
-HAVING COUNT(*) > 1;
